a86349d15278906ce0662aa8c983abfb214c07c1
Francesco Ferretti <ferrettigs@gmail.com>
2023-02-10 15:48:08 -0500

    updates package

--- R/openImage.R
+++ R/openImage.R
@@ -1,15 +1,19 @@
 #' Open image from the sarkPulse archive
 #'
-#' this function require an image name as indicated in the sharkPulse table. Use selectData to get the image names of desired data.
+#' this function require an image name as indicated in the sharkPulse table and the training table. Use selectData to get the image names of desired data.
+#' To work, you need to be able to access the remote server without password
 #' @param img_name name of teh image to open
+#' @param srvUser username in the remote server
 #' @export 
-openImage = function (img_name, baselineUser = "francesco") 
+openImage = function (img_name, srvUser = "francesco") 
 {
 
-    system(paste("scp -P 51001 ", baselineUser, "@sharkpulse.cnre.vt.edu:/srv/shiny-server/pulseMonitor/www/", img_name, ".* .", sep = ""))
+    system(paste("scp ", srvUser, "@sp2.cs.vt.edu:/srv/shiny-server/pulseMonitor/www/", img_name, ".* .", sep = ""))
+    # the other bucket is here /home/spr/sd_images/shark
+    system(paste("scp ", srvUser, "@sp2.cs.vt.edu:/home/spr/sd_images/shark/", img_name, ".* .", sep = ""))
 
     system(paste("open ", img_name, ".*", sep = ""))
 
-    system(paste("rm ", img_name, ".*", sep = ""))
+    #system(paste("rm ", img_name, ".*", sep = ""))
 
 }
